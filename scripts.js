f60e674698a0425cacf58f542c812ed546d8019d
anafonseca05 <a.luizamedeiros05@gmail.com>
2024-03-18 09:55:45 -0300

    formulario

--- scripts.js
+++ scripts.js
@@ -1,7 +1,30 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar);
 var nome = document.getElementById("nome");
+var saida = document.getElementById("saida-de-dados");
+var email = document.getElementById("email");
+var telefone = document.getElementById ("telefone");
+var cep = document.getElementById ("cep");
+var logradouro = document.getElementById ("logradouro");
+var numero = document.getElementById ("numero");
+var complemento = document.getElementById ("complemento");
+var bairro = document.getElementById ("bairro");
+var cidade = document.getElementById ("cidade");
+var estado = document.getElementById ("estado");
+
+
+
 
 function alertar(){
-    alert(nome.value + " " + "Você clicou no botão!!!");
+    //alert(nome.value + " " + "Você clicou no botão!!!");
+    saida.innerText = "Nome: " + " " + nome.value + 
+    "\nE-mail: " + " " + email.value +
+    "\nTelefone: " + " " + telefone.value +
+    "\nCep: " + " " + cep.value +
+    "\nLogradouro: " + " " + logradouro.value +
+    "\nNumero: " + " " + numero.value +
+    "\nComplemento: " + " " + complemento.value +
+    "\nBairro: " + " " + bairro.value +
+    "\nCidade: " + " " + cidade.value +
+    "\nEstado: " + " " + estado.value;
 }
