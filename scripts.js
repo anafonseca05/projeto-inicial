17cb777be5f57da96d25a2b95495a0f9b9684315
anafonseca05 <a.luizamedeiros05@gmail.com>
2024-03-19 09:14:49 -0300

    busca-cep

--- scripts.js
+++ scripts.js
@@ -13,9 +13,21 @@ var cidade = document.getElementById ("cidade");
 var estado = document.getElementById ("estado");
 
 
-
-
 function alertar(){
+
+    //buscar o endereço pelo cep
+    const url =`https://viacep.com.br/ws/${cep.value}/json`;
+
+    fetch(url)
+    .then(resposta=>resposta.json())
+    .then(data => {
+        logradouro.value =data.logradouro;
+        bairro.value = data.bairro;
+        cidade.value = data.cidade;
+        estado.value = data.uf;
+    })
+    .catch(error=>alert(error))
+
     //alert(nome.value + " " + "Você clicou no botão!!!");
     saida.innerText = "Nome: " + " " + nome.value + 
     "\nE-mail: " + " " + email.value +
@@ -27,4 +39,6 @@ function alertar(){
     "\nBairro: " + " " + bairro.value +
     "\nCidade: " + " " + cidade.value +
     "\nEstado: " + " " + estado.value;
+
+    
 }
